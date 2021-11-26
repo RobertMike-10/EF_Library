e1f8bee375fe43bd542b3d65444f2c30db4debeb
Roberto Miguel García Aguirre <61521501+RobertMike-10@users.noreply.github.com>
2021-11-25 18:51:54 -0600

    ForeignKeys

--- WebLibrary/Models/Models/Book.cs
+++ WebLibrary/Models/Models/Book.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,7 +21,13 @@ namespace Models.Models
         [Required]
         public Decimal Price { get; set; }
 
+        [ForeignKey("Category")]
+        public int CategoryId {get;set; }
+        public Category Category { get; set; }
 
+        [ForeignKey("Category")]
+        public int BookDetailId { get; set; }
+        public BookDetail BookDetail { get; set; }
 
     }
 }
---
+++ WebLibrary/Models/Models/BookDetail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Models
+{
+   public class BookDetail
+    {
+        [Key]
+        public int BookDetailId { get; set; }
+        [Required]
+        public int NumberOfChapters { get; set; }
+
+        public int NumberOfPages { get; set; }
+
+        public Decimal Weight { get; set; }
+
+    }
+}
