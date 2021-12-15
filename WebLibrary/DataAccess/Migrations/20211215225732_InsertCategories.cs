7ceff7eb067e161b1d879dbb17bdb45fa08c7629
Roberto Miguel García Aguirre <61521501+RobertMike-10@users.noreply.github.com>
2021-12-15 17:10:21 -0600

    Insert

---
+++ WebLibrary/DataAccess/Migrations/20211215225732_InsertCategories.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class InsertCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categories VALUES ('Enciclopedia');");
+            migrationBuilder.Sql("INSERT INTO Categories VALUES ('Novela');");
+            migrationBuilder.Sql("INSERT INTO Categories VALUES ('Didáctico');");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ WebLibrary/WebLibrary/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using DataAccess.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebLibrary.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
