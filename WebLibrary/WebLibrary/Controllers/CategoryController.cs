cedbc1cce02b52ae32bd25099a316f35b021758a
Roberto Miguel García Aguirre <61521501+RobertMike-10@users.noreply.github.com>
2021-12-16 13:44:53 -0600

    Added Views and Actions for Categories

--- WebLibrary/WebLibrary/Controllers/CategoryController.cs
+++ WebLibrary/WebLibrary/Controllers/CategoryController.cs
@@ -14,10 +14,44 @@ namespace WebLibrary.Controllers
         {
             _db = db;
         }
+        [HttpGet]
         public IActionResult Index()
         {
             List<Category> categories = _db.Categories.ToList();
             return View(categories);
+        }
+        [HttpGet]
+        public IActionResult Upsert(int? id)
+        {
+            Category category = new Category();
+            //
+            if (id == null) return View(category);           
+            category = _db.Categories.FirstOrDefault(c => c.CategoryId == id);
+            if (category == null) return NotFound();
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                if (obj.CategoryId == 0)
+                {
+                    //this is create
+                    _db.Categories.Add(obj);
+                }
+                else
+                {
+                    //this is an update
+                    _db.Categories.Update(obj);
+                }
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(obj);
+
         }
     }
 }
