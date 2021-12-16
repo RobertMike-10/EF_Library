e853cc38a413965f9e5c39a22ebed23b0645472b
Roberto Miguel García Aguirre <61521501+RobertMike-10@users.noreply.github.com>
2021-12-16 16:59:38 -0600

    Added Author Controller

---
+++ WebLibrary/WebLibrary/Controllers/AuthorController.cs
@@ -0,0 +1,68 @@
+﻿using DataAccess.Data;
+using Microsoft.AspNetCore.Mvc;
+using Models.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebLibrary.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AuthorController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Author> objList = _db.Authors.ToList();
+            return View(objList);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Author author = new Author();
+            if (id == null) return View(author);
+           
+            //this for edit
+            author = _db.Authors.FirstOrDefault(a => a.AuthorId == id);
+            if (author == null) return NotFound();
+            
+            return View(author);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Author author)
+        {
+            if (ModelState.IsValid)
+            {
+                if (author.AuthorId == 0)
+                {
+                    //this is create
+                    _db.Authors.Add(author);
+                }
+                else
+                {
+                    //this is an update
+                    _db.Authors.Update(author);
+                }
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(author);
+
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var author = _db.Authors.FirstOrDefault(a => a.AuthorId == id);
+            _db.Authors.Remove(author);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+
+}
