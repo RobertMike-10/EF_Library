e61aa35e4897e22f0d4f7f1abdf4c3f5176f8c09
Roberto Miguel García Aguirre <61521501+RobertMike-10@users.noreply.github.com>
2021-12-16 14:20:44 -0600

    Adding Records

--- WebLibrary/WebLibrary/Controllers/CategoryController.cs
+++ WebLibrary/WebLibrary/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using DataAccess.Data;
 using Microsoft.AspNetCore.Mvc;
 using Models.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -24,9 +25,10 @@ namespace WebLibrary.Controllers
         public IActionResult Upsert(int? id)
         {
             Category category = new Category();
-            //
+            //Create
             if (id == null) return View(category);           
             category = _db.Categories.FirstOrDefault(c => c.CategoryId == id);
+            //Category Not Found
             if (category == null) return NotFound();
             return View(category);
         }
@@ -52,6 +54,27 @@ namespace WebLibrary.Controllers
             }
             return View(obj);
 
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var category= _db.Categories.FirstOrDefault(c => c.CategoryId == id);
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult CreateMultiple(int id)
+        {
+            List<Category> catList = new List<Category>();
+            for (int i = 1; i <= id; i++)
+            {
+                catList.Add(new Category { Name = Guid.NewGuid().ToString() });
+                //_db.Categories.Add(new Category { Name = Guid.NewGuid().ToString() });
+            }
+            _db.Categories.AddRange(catList);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
